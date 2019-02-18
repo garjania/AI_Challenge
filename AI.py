6d693eafa99933787438aba88ce44b61c359799e
Kooshan1382 <kooshan.mk@gmail.com>
2019-02-18 23:56:57 +0330

    hero updated

--- AI.py
+++ AI.py
@@ -5,10 +5,20 @@ from Game import Search
 
 
 class AI:
-
     counter = 0
-
-    def preprocess(self, world):
+    zone = []
+    go=[]
+    def preprocess(self, world,):
+        for r in world.map.cells:
+            for cell in r:
+                if cell.is_in_objective_zone and not cell.is_wall:
+                    row = cell.row
+                    col = cell.column
+                    self.zone.append([row, col])
+        for i in range(4):
+            row = self.zone[randint(0, len(self.zone) - 1)][0]
+            col = self.zone[randint(0, len(self.zone) - 1)][1]
+            self.go.append([row,col])
         print("preprocess")
 
     def pick(self, world):
@@ -23,14 +33,12 @@ class AI:
 
     def move(self, world):
         print("move")
-        for r in world.map.cells:
-            for cell in r:
-                if cell.is_in_objective_zone and not cell.is_wall:
-                    row = cell.row
-                    col = cell.column
-                    break
+        i=0
         for hero in world.my_heroes:
+            row=self.go[i][0]
+            col=self.go[i][1]
             world.move_hero(hero=hero, direction=Search().search(hero, world.map, row, col))
+            i+=1
     def action(self, world):
         print("action")
         for hero in world.my_heroes:
@@ -38,7 +46,7 @@ class AI:
             col_num = randint(0, world.map.column_num)
             abilities = hero.abilities
             world.cast_ability(hero=hero, ability=abilities[randint(0, len(abilities) - 1)],
-                               cell=world.map.get_cell(row_num, col_num))
+            cell=world.map.get_cell(row_num, col_num))
 
 
 
